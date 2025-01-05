906bc56ed06e9456a864dee3a3e7c7b9290f3df5
Ahmad Barclay <Ahmad.Barclay@ons.gov.uk>
2025-01-05 22:34:57 +0000

    removed stray punctuation

--- src/lib/Credit.svelte
+++ src/lib/Credit.svelte
@@ -8,7 +8,7 @@
 		href="https://bsky.app/profile/bothness.bsky.social">Ahmad Barclay</a
 	>, inspired by Eugene Alvin Villar's
 	<a target="_blank" href="https://seav.github.io/osm-15-puzzle/">OSM 15 Puzzle</a>. Area search
-	powered by <a target="_blank" href="https://nominatim.org/">Nominatim</a>. {@html attribution}.
+	powered by <a target="_blank" href="https://nominatim.org/">Nominatim</a>. {@html attribution}
 	Get the <a target="_blank" href="https://github.com/bothness/anymap/">source code</a>
 	on Github.
 </p>
